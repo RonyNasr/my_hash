6e6a2e4ebe401e77bfcef6693bd28cd7915c1080
Caleb Stokka and Rony Nasr <caleb+rony@>
2016-07-05 16:19:19 -0700

    working hash

--- lib/my_hash.rb
+++ lib/my_hash.rb
@@ -2,14 +2,14 @@ require('pry')
 
 class MyHash
   define_method(:initialize) do
-    @my_hash = Hash.new()
+    @my_hash = {}
   end
 
   define_method(:[]) do |key|
     @my_hash[key]
   end
 
-  define_method(:my_store) do |key,value|
+  define_method(:my_store) do |key, value|
     @my_hash[key] = value
   end
 
--- spec/my_hash_spec.rb
+++ spec/my_hash_spec.rb
@@ -6,6 +6,7 @@ describe(MyHash) do
   describe('#my_store') do
     it('add a key and value to a hash') do
       test_hash = MyHash.new()
+      # binding.pry
       expect(test_hash.my_store("key1","value1")).to(eq("value1"))
     end
   end
@@ -14,6 +15,7 @@ describe(MyHash) do
     it('add a key and value to a hash') do
       test_hash = MyHash.new()
       test_hash.my_store("key1","value1")
+      binding.pry
       expect(test_hash.my_fetch("key1")).to(eq("value1"))
     end
   end
